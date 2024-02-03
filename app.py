c660d3aa9d6334a918a4078b88b52e734ee15501
Jason Wu <jwu0592@gmail.com>
2024-02-03 15:07:03 -0500

    added modularity for the google credentials

--- app.py
+++ app.py
@@ -10,23 +10,7 @@ img_text_arr = img_text.split(" ")
 app = Flask(__name__)
 @app.route('/')
 def index():
-    # # detect text from image file in argument
-    # img_text = detect_text('./Text Samples/German_Text.png')
-
-    # # translate the text into english
-    # translated_text = translate_text(img_text)
-
-    # # split the translated text into an array of words
-    # translate_text_arr = translated_text.split(" ")
-
-    # # convert html entities to ascii in the array
-    # translate_text_arr = convert_html_entites_to_ascii(translate_text_arr)
-
-    # # print statments for debugging in python only (NOT for the web app)
-    # # print(translate_text_arr)
-    # # print(detect_language(img_text))
-
-    # # render the index.html template with the translated text array
+    
     return render_template('index.html')
 
 if __name__ == '__main__':
