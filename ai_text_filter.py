c9e5386b8e287ac5e541b48a1d6257dc3763934f
3kstr3m3 <planetaidan@gmail.com>
2024-02-03 23:35:28 -0500

    flashcards are now integrated with ai text filtering

--- ai_text_filter.py
+++ ai_text_filter.py
@@ -5,8 +5,10 @@ co = cohere.Client(API_credentials.cohere())
 
 def filter_text(input):
     response = co.generate(
-    prompt='Pick out key words from the following text that would be useful for someone studying English. Only output the words with no other text. The words should not contain spaces, and should each be on their own line with no bullet points:\n'
+    prompt='Pick out key words from the following text that would be useful for someone studying English. Only output the list of words with no other text before or after. The words should not contain spaces, and should each be on their own line with no bullet points:\n\n'
         + input,
     temperature=0.0
     )
-    return response
+    return response[0]
+
+#print(filter_text("bob went to the store, but he got hungry and went home"))
