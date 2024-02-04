98e8940440284fb4efed9a2530cd351346a3799c
3kstr3m3 <planetaidan@gmail.com>
2024-02-04 01:32:15 -0500

    fixed a bug due to weird google translate prompting in text_translator.py and added proper filtering in the API credentials

--- app.py
+++ app.py
@@ -3,42 +3,46 @@ from flask import Flask, render_template, jsonify
 import text_translator
 import ai_text_filter as atf
 
-# img_text = text_translator.get_img_text('./Text Samples/Chinese_Text.png')
-# translate_text_arr = text_translator.get_translated_text(img_text)
-# img_text_arr = img_text.split(" ")
-js_objects = []
-img_text_arr = ['李', '叶', '的', '爷爷', '经常', '在', '外面', '很少', '在家', '李'] 
-translate_text_arr = ['Li', "Ye's", 'grandfather', 'is', 'often', 'away', 'from', 'home', 'home.', 'plum']
+img_text = text_translator.get_img_text('./Text Samples/German_Text.png')
+translate_text_arr = text_translator.get_translated_text(img_text)
+img_text_arr = img_text.split(" || ")
+
+print(img_text)
+print(img_text_arr)
+print(translate_text_arr)
 
 # calls cohere api to filter the key words and parses them in to an array
-translate_text_str = ' '.join(map(str, translate_text_arr))
+translate_text_str = '\n'.join(map(str, translate_text_arr))
 filtered_text = atf.filter_text(translate_text_str).lower()
 words = filtered_text.split('\n')
 filtered_list = [word.strip() for word in words if word.strip()]
 
+# for x in filtered_list:
+#     x.replace(" ", "")
+#     x.replace(".", "")
+#     if x[0] == "-":
+#         x = x[1:]
+# print(filtered_list)
+
 # locates the original word from the english translation and puts it in a list
-filtered_list_foreign = []
-count = 0
-deletion_list = []
-for word in filtered_list:
-    try:
-        filtered_list_foreign.append(img_text_arr[translate_text_arr.index(word)])
-    except:
-        deletion_list.append(count)
-        count -= 1
-    count += 1
-
-# deletes all items not in the original text (faulty translations) 
-for x in deletion_list:
-    del filtered_list[x]
-
+front_flashcards = []
+back_flashcards = []
 for i in range(len(filtered_list)):
-    print(str(filtered_list[i]) + " = " + str(filtered_list_foreign[i]))
+    try:
+        pos = translate_text_arr.index(filtered_list[i])
+        front_flashcards.append(img_text_arr[pos])
+        back_flashcards.append(translate_text_arr[pos])
+    except:
+        print(filtered_list[i] + " could not be found in the original")
 
-for filtered_list_foreign, filtered_list in zip(filtered_list_foreign, filtered_list):
+for i in range(len(front_flashcards)):
+    print(str(front_flashcards[i]) + " = " + str(back_flashcards[i]))
+
+js_objects = []
+for front_flashcards, back_flashcards in zip(front_flashcards, back_flashcards):
     js_object = {
-        'question': filtered_list_foreign,
-        'answer': filtered_list
+        'question': front_flashcards,
+        'answer': back_flashcards
     }
     js_objects.append(js_object)
 
